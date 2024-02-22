4e0b0646c06aeb809fdc98b6e2ddb877e3d824a3
Kristian Tsivkov <109758711+ktsivkov@users.noreply.github.com>
2024-02-22 14:52:21 +0100

    Implemented valueToString function used to generate the query identifier taking into account pointer-case query args (#13)
    Co-authored-by: dennis-dko <119768938+dennis-dko@users.noreply.github.com>

--- identifier_test.go
+++ identifier_test.go
@@ -18,3 +18,38 @@ func Test_buildIdentifier(t *testing.T) {
 		t.Errorf("buildIdentifier expected to return `%s` but got `%s`", expected, actual)
 	}
 }
+
+func Test_sliceToString(t *testing.T) {
+	expected := "[test-val test-val 1 1 true true [test-val] [1] [true] [test-val] [1] [true] [test-val] [1] [true] [test-val] [1] [true] {test-val: test-val} {1: 1} {true: true} {test-val: test-val} {1: 1} {true: true} {test-val: test-val} {1: 1} {true: true} {test-val: test-val} {1: 1} {true: true}]"
+
+	strVal := "test-val"
+	intVal := 1
+	boolVal := true
+	sliceOfStr := []string{strVal}
+	sliceOfInt := []int{intVal}
+	sliceOfBool := []bool{boolVal}
+	sliceOfPointerStr := []*string{&strVal}
+	sliceOfPointerInt := []*int{&intVal}
+	sliceOfPointerBool := []*bool{&boolVal}
+	mapOfStr := map[string]string{strVal: strVal}
+	mapOfInt := map[int]int{intVal: intVal}
+	mapOfBool := map[bool]bool{boolVal: boolVal}
+	mapOfPointerStr := map[*string]*string{&strVal: &strVal}
+	mapOfPointerInt := map[*int]*int{&intVal: &intVal}
+	mapOfPointerBool := map[*bool]*bool{&boolVal: &boolVal}
+	actual := valueToString([]interface{}{
+		strVal, &strVal, intVal, // Primitives
+		&intVal, boolVal, &boolVal, // Pointer passed primitives
+		sliceOfStr, sliceOfInt, sliceOfBool, // Slices of primitives
+		&sliceOfStr, &sliceOfInt, &sliceOfBool, // Pointer passed slices of primitives
+		sliceOfPointerStr, sliceOfPointerInt, sliceOfPointerBool, // Slices of pointer primitives
+		&sliceOfPointerStr, &sliceOfPointerInt, &sliceOfPointerBool, // Pointer passed slices of pointer primitives
+		mapOfStr, mapOfInt, mapOfBool, // Map of primitives
+		mapOfPointerStr, mapOfPointerInt, mapOfPointerBool, // Map of pointer primitives
+		&mapOfStr, &mapOfInt, &mapOfBool, // Map of primitives
+		&mapOfPointerStr, &mapOfPointerInt, &mapOfPointerBool, // Map of pointer primitives
+	})
+	if expected != actual {
+		t.Errorf("sliceToString expected to return `%s` but got `%s`", expected, actual)
+	}
+}
