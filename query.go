b327816af75d6c3992862b768856f5cc5cd677f5
ktsivkov <ktsivkov@gmail.com>
2024-03-21 17:57:59 +0100

    Resolve a suspicious race condition when doing Unmarshal on the Query struct

--- query.go
+++ query.go
@@ -2,6 +2,7 @@ package caches
 
 import (
 	"encoding/json"
+	"sync"
 
 	"gorm.io/gorm"
 )
@@ -9,17 +10,24 @@ import (
 type Query[T any] struct {
 	Dest         T
 	RowsAffected int64
+	mu           sync.RWMutex
 }
 
 func (q *Query[T]) Marshal() ([]byte, error) {
+	q.mu.RUnlock()
+	defer q.mu.RUnlock()
 	return json.Marshal(q)
 }
 
 func (q *Query[T]) Unmarshal(bytes []byte) error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
 	return json.Unmarshal(bytes, q)
 }
 
 func (q *Query[T]) replaceOn(db *gorm.DB) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
 	SetPointedValue(db.Statement.Dest, q.Dest)
 	SetPointedValue(&db.Statement.RowsAffected, &q.RowsAffected)
 }
